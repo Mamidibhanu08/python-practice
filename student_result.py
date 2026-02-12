aaee62024177c96b54c1f54071718463b214a9de
Mamidibhanu08 <117571419+Mamidibhanu08@users.noreply.github.com>
2026-02-12 16:23:50 +0530

    Create student_result.py

---
+++ student_result.py
@@ -0,0 +1,8 @@
+sub1=int(input("enter marks sub1:"))
+sub2=int(input("enter marks sub2:"))
+sub3=int(input("enter marks sub3:"))
+
+if sub1>=35 and sub2>=35 and sub3>=35:
+    print("pass")
+else:
+    print("fail")
