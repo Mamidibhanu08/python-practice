f9117a6487a6918a02454a22c15fa707a491150d
Mamidibhanu08 <117571419+Mamidibhanu08@users.noreply.github.com>
2026-02-18 15:41:58 +0530

    Create prac_questions.py

---
+++ prac_questions.py
@@ -0,0 +1,13 @@
+Python practice qns
+
+name=input("enter name:")
+age=int(input("enter age:"))
+print(f"hello {name},you are {age} old")
+
+i=int(input("enter num1:"))
+j=int(input("enter num2:"))
+print(f"sum is{i+j}")
+print(f"mul is{i*j}")
+print(f"diff is{i-j}")
+print(f"div is{i/j}")
+        
